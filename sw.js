ee0c6265496dd005d62939fa350d5e698664a0c0
KnicKnic <35521284+KnicKnic@users.noreply.github.com>
2018-07-28 23:04:00 -0700

    enable service worker

--- sw.js
+++ sw.js
@@ -1,30 +1,29 @@
 // version 1
 // v0
-var CACHE_NAME = 'cache_1_0';
+var prefix_name = "croppy_"
+var CACHE_NAME = prefix_name + 'cache_1_0';
 var cacheWhitelist = [CACHE_NAME];
-// var urlsToCache = [
-//     '/',
-//     '/index.html',
-//     '/images/croppy.png',
-//     '/manifest.json',
-//     '/magick.js',
-//     '/magick.wasm',
-//     '/magickApi.js',
-//     '/sw.js',
-//     '/instructions.html',
-//    // '/copyright.html',
-//     'croppy_fav.png',
-//     'https://cdn.jsdelivr.net/g/filesaver.js',
-//     'https://cdnjs.cloudflare.com/ajax/libs/jszip/3.1.5/jszip.min.js'
-// ];
-var urlsToCache=[];
+var urlsToCache = [
+    './',
+    './index.html',
+    './images/croppy.png',
+    './manifest.json',
+    './magick.js',
+    './magick.wasm',
+    './magickApi.js',
+    './instructions.html',
+    './favicon.png',
+    'https://cdn.jsdelivr.net/g/filesaver.js',
+    'https://cdnjs.cloudflare.com/ajax/libs/jszip/3.1.5/jszip.min.js'
+];
+// var urlsToCache=[];
 
 self.addEventListener('activate', function(event) {  
     event.waitUntil(
       caches.keys().then(function(cacheNames) {
         return Promise.all(
           cacheNames.map(function(cacheName) {
-            if (cacheWhitelist.indexOf(cacheName) === -1) {
+            if (cacheName.startsWith(prefix_name) && cacheWhitelist.indexOf(cacheName) ===-1) {
                 //alert("asdf")
                 //self.postMessage("deleting cache " + cacheName)
               return caches.delete(cacheName);
